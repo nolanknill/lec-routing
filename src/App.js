fbfb87756b2ee823826484d466055c7646001aee
Nolan Knill <nolanknill@gmail.com>
2022-11-03 14:27:38 -0400

    Naive client-side router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import BluePage from "./pages/BluePage/BluePage";
 import RedPage from "./pages/RedPage/RedPage";
 import YellowPage from "./pages/YellowPage/YellowPage";
+import { useState } from "react";
+import "./App.scss";
 
 function App() {
+  // What might currentPage hold? 
+  const initialPageValue = window.location.pathname.split("/")[1];
+  const [currentPage, setCurrentPage] = useState(initialPageValue || "red");
+ 
+  const handleClick = (page) => {
+    setCurrentPage(page);
+
+    window.history.pushState({}, "", `/${page}`);
+  }
+
   return (
     <>
-      <RedPage />
-      <BluePage />
-      <YellowPage />
+      <ul className="nav">
+        <li onClick={ () => { handleClick("red"); }}>Red</li>
+        <li onClick={ () => { handleClick("blue"); }}>Blue</li>
+        <li onClick={ () => { handleClick("yellow"); } }>Yellow</li>
+      </ul>
+      {currentPage === 'red' && <RedPage />}
+      {currentPage === 'blue' && <BluePage />}
+      {currentPage === 'yellow' && <YellowPage />}
     </>
   );
 }
