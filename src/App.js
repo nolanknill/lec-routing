a7510a643068552308a45b517a797325a8abc40c
Nolan Knill <nolanknill@gmail.com>
2022-11-03 17:37:10 -0400

    Demo code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import BluePage from "./pages/BluePage/BluePage";
 import RedPage from "./pages/RedPage/RedPage";
 import YellowPage from "./pages/YellowPage/YellowPage";
+import { useState } from "react";
 
 function App() {
+  const selectedPage = window.location.pathname.split("/")[1];
+
+  const [currentPage, setCurrentPage] = useState(selectedPage || "red");
+  
+  const goToPage = (page) => {
+    setCurrentPage(page);
+
+    // update the browser history object!
+    window.history.pushState({}, "", `/${page}`);
+  }
+
   return (
     <>
-      <RedPage />
-      <BluePage />
-      <YellowPage />
+      <nav>
+        <ul>
+          <li onClick={ () => {goToPage("red") }}>Red</li>
+          <li onClick={ () => {goToPage("blue") }}>Blue</li>
+          <li onClick={ () => {goToPage("yellow") }}>Yellow</li>
+        </ul>
+      </nav>
+      
+      {currentPage === "red" && <RedPage />}
+      {currentPage === "blue" && <BluePage /> }
+      {currentPage === "yellow" && <YellowPage />}
     </>
   );
 }
